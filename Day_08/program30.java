db7b2270c6b6b06924470c7ff3e1446cbd8d8ed5
saksham-mishra-glbitm <kenzydope37@gmail.com>
2026-06-06 12:41:38 +0530

    4 JUNE: Day-8 Uploaded

---
+++ Day_08/program30.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class program30
+{
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] sm)
+    {
+        System.out.println("Enter the size of pattern:");
+        int n=sc.nextInt();
+
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+                System.out.print((j+1)+" ");
+            
+            System.out.println();
+        }
+    }
+}
